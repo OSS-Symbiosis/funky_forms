0269c1bce62660a66fd6eae908ad153453693dbb
Siddharth Khatri <siddxharth@gmail.com>
2022-07-29 18:47:22 +0530

    Add date and style to hero section

--- src/Hero.js
+++ src/Hero.js
@@ -2,11 +2,14 @@ import React from 'react';
 import './styles/hero.css';
 
 export default function Hero() {
+  const date = "Friday, 5th August 2022";
   return (
     <div className='hero-container'>
         <h1>SOSS</h1>
-        <h2 id='speaker' >Kumar Shivam</h2>
         <h2>Tech-talk</h2>
+        <p className='by'>by</p>
+        <h2 id='speaker' >Kumar Shivam</h2>
+        <h2 className='date'>{date}</h2>
     </div>
   )
 }
