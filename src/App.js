53f86243ae11dba23d2ad8b5acaa4d0834c387b1
Siddharth Khatri <siddxharth@gmail.com>
2022-07-29 20:05:44 +0530

    Add router-dom

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import Hero from './Hero';
 import FormikForm from './formik';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 
 function App() {
   return (
-    <div>
-      <Hero />
-      <FormikForm />
-    </div>
+    <Router>
+      <Routes>
+        <Route path={`/register/`} element={<div><Hero/><FormikForm /></div>} />
+        <Route path='*' element={<div>[404] Page doesn't exist! Are you visiting the right link?</div>} />
+      </Routes>
+    </Router>
   );
 }
 
