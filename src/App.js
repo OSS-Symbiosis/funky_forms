17b59c30f9d06d745a5a2a976245f7dc3f7bf079
Ishan Siddiqui <ishansiddiqui123@gmail.com>
2022-09-05 21:04:32 +0530

    Added 2 login forms login2 is working properly, please check once...

--- src/App.js
+++ src/App.js
@@ -2,16 +2,14 @@ import Hero from './Hero';
 import FormikForm from './formik';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import NotFound from './NotFound';
-
+import Login from './Login';
+import Login2 from './Login2';
 
 function App() {
+
   return (
-    <Router>
-      <Routes>
-        {/* <Route path={`/register/`} element={<div><Hero/><FormikForm /></div>} /> */}
-        <Route path='*' element={<NotFound />} />
-      </Routes>
-    </Router>
+    <Login2/>
+    
   );
 }
 
